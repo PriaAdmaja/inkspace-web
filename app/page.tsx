1edd12e728697ccf1bd43115df7ae0a9c5bdf480
Pria Admaja <priaadmaja@gmail.com>
2026-02-05 13:41:26 +0700

    Adjust post list

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 import { auth } from "@/auth";
 import { getPostsData } from "@/lib/data/posts";
 import { Post } from "@/types/posts";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import dayjs from "dayjs";
 
 export default async function Home() {
@@ -16,20 +16,19 @@ export default async function Home() {
         <Header user={session?.user} />
       </Suspense>
 
-      <section className="p-10 flex flex-col gap-4">
+      <section className="p-10 flex flex-col gap-4 max-w-7xl mx-auto">
         {data?.map((post: Post) => {
           const content = post.content.split(" ").slice(0, 70).join(" ")
           return (
-            <Card key={post.id}>
-              <CardHeader>
+            <Card key={post.id} className="gap-3">
+              <CardHeader className="gap-0">
                 <CardTitle className="text-2xl font-bold">{post.title}</CardTitle>
-                <CardDescription>by {post.author.username}</CardDescription>
               </CardHeader>
               <CardContent>
                 <p>{content}{post.content.length > 70 && "..."}</p>
               </CardContent>
               <CardFooter>
-                <p className="text-muted-foreground text-sm">{dayjs(post.createdAt).format("MMM DD, YYYY")}</p>
+                <p className="text-muted-foreground text-sm">{dayjs(post.createdAt).format("MMM DD, YYYY")} • {post.author.username}</p>
               </CardFooter>
             </Card>
           )
